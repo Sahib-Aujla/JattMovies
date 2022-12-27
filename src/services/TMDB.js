f2375aa98f39778dc2b3f3ff13ef8472ba1c148f
Sahib-Aujla <sahibpreet568@gmail.com>
2022-12-27 13:15:11 -0500

    Redux store setup

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -1,6 +1,17 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+const tmdbKey = process.env.REACT_APP_TMDB_KEY;
+// const tmdbKey = 'f0d8d185289a6d531ff4fc4844a95f75';
+const page = 1;
+// https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1
 export const tmdbApi = createApi({
   reducerPath: 'tmdbApi',
-  baseQuery: fetchBaseQuery({ baseUrl: '' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.themoviedb.org/3' }),
+  endpoints: (builder) => ({
+    getMovies: builder.query({
+      query: () => `movie/popular?api_key=${tmdbKey}&page=${page}`,
+    }),
+  }),
 });
+
+export const { useGetMoviesQuery } = tmdbApi;
